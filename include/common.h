43cfbb979dc4b6db4e41fecb322520e40eb68a40
Ciaran Anscomb <www@6809.org.uk>
2025-02-06 16:05:11 +0000

    ensure i8 is signed char

--- include/common.h
+++ include/common.h
@@ -2,10 +2,10 @@
 #define COMMON_H
 
 
-typedef char    i8;
-typedef short   i16;
-typedef int     i32;
-typedef long    i64;
+typedef signed char i8;
+typedef short       i16;
+typedef int         i32;
+typedef long        i64;
 
 typedef unsigned char    u8;
 typedef unsigned short   u16;
